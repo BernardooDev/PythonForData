1af76b0ed3cc056f797b91924035458352ac4aea
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2026-07-21 12:30:17 -0300

    Modify the value of the object

---
+++ 3-FormattedDate/Api.py
@@ -0,0 +1,13 @@
+produto = {
+    "nome": "Notebook",
+    "preco": 4500,
+    "estoque": 18
+}
+
+print(produto["nome"])
+print(produto["preco"])
+print(produto["estoque"])
+
+produto["estoque"] = 20
+
+print(produto["estoque"])
